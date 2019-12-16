b87c93330256f818bdef4056d2c522933196f7e7
Daria Gilletti <daria.gilletti@gmail.com>
2019-12-16 17:59:27 +0100

    Fixed user.service

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -7,13 +7,13 @@ import { UserService } from '../user.service';
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
+
 export class ListComponent implements OnInit {
-  // @Input() users: User[];
+  public users: User[];
   constructor(private userService: UserService) { }
 
   async ngOnInit() {
     const users = await this.userService.getUsers();
     console.log('users:', users);
   }
-
 }
