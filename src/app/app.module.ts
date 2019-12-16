b291f295ce7e4ad6384bf20a806fe5fa08a7c907
Daria Gilletti <daria.gilletti@gmail.com>
2019-12-16 12:37:40 +0100

    Created angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
