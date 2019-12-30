ffa7fd85ae2daa3eddff843b02e986fcb18cdbf7
erocookv5 <matteodicaragm@gmail.com>
2019-12-30 18:44:49 +0100

    login on navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ChatComponent } from './chat/chat.component';
 import { ListComponent } from './list/list.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'chats', component: ListComponent },
-  { path: 'chats/:id', component: ChatComponent }
+  { path: 'chats/:id', component: ChatComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
